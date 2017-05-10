1b28668178f5995d34514b6dd5fd9b2cfad9d91b
Lorenzo Parenti <lorenzo@flowmo.co>
2017-05-10 13:47:14 +0100

    Update not found page

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -5,7 +5,7 @@ export default () => {
     return (
       <div className="boxed-view">
         <div className="boxed-view__box">
-          <h1>Page Not Found</h1>
+          <h1>404 - Page Not Found</h1>
           <p>Hey-Diddly-Ho! We're unable to find that page.</p>
           <Link className="button button--link" to="/">Head Home</Link>
         </div>
